fb47df2a8240c3c7abc304b4c64fc5be3fac069c
EFFM00 <fischietto.elena@gmail.com>
2024-01-01 01:22:22 -0300

    avances

--- src/main.jsx
+++ src/main.jsx
@@ -7,40 +7,33 @@ import Login from './pages/Login/Login.jsx';
 import Register from './pages/Register/Register.jsx';
 import Dashboard from './pages/Dashboard/Dashboard.jsx';
 import AuthLayout from './components/AuthLayout.jsx';
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<h1>HOLA 1</h1>}/>
-          <Route path="/login" element={
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<h1>HOLA 1</h1>} />
+            <Route path="/login" element={
               <AuthLayout authentication={false}>
                 <Login />
               </AuthLayout>
-            }
-          />
+            } />
 
-          <Route path="/register"
-            element={
+            <Route path="/register" element={
               <AuthLayout authentication={false}>
                 <Register />
               </AuthLayout>
-            }
-          />
+            } />
 
-          <Route path="/dashboard/*"
-            element={
-              <AuthLayout authentication={true}>
-                <Dashboard />
-              </AuthLayout>
-            }
-          >
-            <Route index element={<>HOLAAAAAAAA index</>} />
-          </Route>
+            <Route path="/dashboard/*" element={<p>HOLA p</p>}>
+              <Route index element={<Dashboard />}/>
+              <Route path="h" element={<h1>HOLAAAAAAAA index</h1>} />
+            </Route>
 
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 );
